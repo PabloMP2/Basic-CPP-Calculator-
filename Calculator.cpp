5e7e90747bc852d88c1ed31e458e639d570fd07c
PabloMP2 <109240920+PabloMP2@users.noreply.github.com>
2022-10-04 19:49:48 +0100

    Update Calculator.cpp
    Added Much More Too The Code Made It Slightly Less Basic

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,24 +1,61 @@
-
-// stringstreams
 #include <iostream>
 #include <string>
 #include <sstream>
 using namespace std;
 
-int main()
-{
-	string mystr;
-	float price = 0;
-	int64_t quantity = 0;
-	cout << "What's your name? ";
-	getline(cin, mystr);
+int main() {
+  string mystr;
+  string iguhgj;
+  static float price = 0;
+  static u_int64_t quantity = 0;
+  cout << "What's your name? ";
+  getline(cin, mystr);
+  
 	cout << "Hello " << mystr << ".\n";
-	cout << "Enter The Number In Which I Will Times: ";
-	getline(cin, mystr);
 	stringstream(mystr) >> price;
-	cout << "Enter How Many Times I Should Times It: ";
-	getline(cin, mystr);
+  
+  cout << "Number: ";
+  getline(cin, mystr);
+
+
+  cout << "Add Times Or Subtract Or Divide: ";
+  getline(cin, iguhgj);
+	stringstream(mystr) >> price;
+  
+  
+  if(iguhgj == "+"){
+    cout << "Enter How Many Times I Should Add It: ";
+    getline(cin, mystr);
+	  stringstream(mystr) >> quantity;
+    cout << "Total: " << price + quantity << endl;
+  }
+    
+	else if(iguhgj == "-"){
+    cout << "Enter How Many Times I Should Take It: ";
+    	getline(cin, mystr);
 	stringstream(mystr) >> quantity;
-	cout << "Total: " << price * quantity << endl;
-	return 0;
+    cout << "Total: " << price - quantity << endl;
+  }
+    
+  else if(iguhgj == "*"){
+    cout << "Enter How Many Times I Should Times It: ";
+    	getline(cin, mystr);
+	stringstream(mystr) >> quantity;
+    cout << "Total: " << price * quantity << endl;
+  }
+    else if(iguhgj == "/"){
+    cout << "Enter How Many Times I Should Times It: ";
+    	getline(cin, mystr);
+	stringstream(mystr) >> quantity;
+    cout << "Total: " << price / quantity << endl;
+  }
+
+
+
+  
+	//cout << "Total: " << price * quantity << endl;
+  //cout << "Enter The Number In Which I Will Times: ";
+	//getline(cin, mystr);
+  //
+    
 }
